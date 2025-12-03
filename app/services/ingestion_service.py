07cc1333647770239424cdc7b784473ccb3e3322
Draxer542 <95914976+Draxer542@users.noreply.github.com>
2025-12-03 22:51:27 +0530

    Update GenerativeAIEmbeddings with HuggingFaceEmbeddings

--- app/services/ingestion_service.py
+++ app/services/ingestion_service.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import UploadFile, HTTPException
 from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader, UnstructuredMarkdownLoader
 from langchain_experimental.text_splitter import SemanticChunker
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 from app.core.config import settings
@@ -16,7 +16,7 @@ MAX_FILES = 3
 
 class IngestionService:
     def __init__(self):
-        self.embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=settings.GOOGLE_API_KEY)
+        self.embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
         self.vector_store = Chroma(
             collection_name="agentic_rag",
             embedding_function=self.embeddings,
